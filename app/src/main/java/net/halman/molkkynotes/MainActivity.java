7bc15443c0f433f6d4084960ab264cca7b6f4363
Tomas Halman <tomas@halman.net>
2020-06-05 17:30:52 +0200

    Problem: Unused fab button in activity
    Solution: Remove it

--- app/src/main/java/net/halman/molkkynotes/MainActivity.java
+++ app/src/main/java/net/halman/molkkynotes/MainActivity.java
@@ -68,16 +68,6 @@ public class MainActivity extends AppCompatActivity
             public void onTabReselected(TabLayout.Tab tab) { }
         });
 
-        FloatingActionButton fab = findViewById(R.id.fabMail);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         _players.load(this);
