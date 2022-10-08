92bc1b4d2009822e80fa15894b84bb29313ffa94
Tomas Halman <tomas@halman.net>
2022-10-08 11:27:21 +0200

    Catch onInterceptTouchEvent exception on viewPager
    To be on safe side catch also exception comming from
    onInterceptTouchEvent call.

--- app/src/main/java/net/halman/molkkynotes/ui/main/SafeViewPager.java
+++ app/src/main/java/net/halman/molkkynotes/ui/main/SafeViewPager.java
@@ -1,5 +1,6 @@
 package net.halman.molkkynotes.ui.main;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -12,10 +13,19 @@ public class SafeViewPager extends ViewPager {
     public SafeViewPager(@NonNull Context context) { super(context); }
     public SafeViewPager(@NonNull Context context, @Nullable AttributeSet attrs) { super(context, attrs); }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         try {
             return super.onTouchEvent(ev);
+        } catch (Exception ignore) {}
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        try {
+            return super.onInterceptTouchEvent(event);
         } catch (Exception ignore) {}
         return false;
     }
